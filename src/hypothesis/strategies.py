8d361504cfa964bb7c52e29f8a263d6ec289b29f
David R. MacIver <david@drmaciver.com>
2017-08-13 09:23:58 +0100

    Remove the notion of bind points

--- src/hypothesis/strategies.py
+++ src/hypothesis/strategies.py
@@ -1247,8 +1247,6 @@ def composite(f):
                 first_draw = [True]
 
                 def draw(strategy):
-                    if not first_draw[0]:
-                        data.mark_bind()
                     first_draw[0] = False
                     return data.draw(strategy)
 
@@ -1396,7 +1394,6 @@ def data():
             return 'data(...)'
 
         def draw(self, strategy, label=None):
-            self.data.mark_bind()
             result = self.data.draw(strategy)
             self.count += 1
             if label is not None:
